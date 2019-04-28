a0b506b7310dd80d9de3eb1d1cce2d38f5dcb1e1
saisree1608 <saisree.pokala@gmail.com>
2019-04-28 17:31:44 +0530

    Modified_2 Direct.java

--- Direct.java
+++ Direct.java
@@ -81,25 +81,6 @@ class Dire extends JFrame implements ActionListener{
 		p2.add(l10);
 		p2.add(b10);
 	}
-	/*Dire(){
-		p1 = new JPanel();
-		frr = new JFrame();
-		p1.setLayout(new BorderLayout());
-		JEditorPane web = new JEditorPane();
-		web.setContentType("text/html");
-		web.setEditable(false);
-		web.setText("<h1>STUDENT INFORMATION</h1><h2 style=\"font-weight:normal;\">NAME : SUDHEENDRA RAGHAV<br><br>DOB : 30-10-1994<br><br>PRESENT ADDRESS : Flat no. A7-304, Brigade Millenium, JP Nagar, Phase 7, Bengaluru.PIN : 560078.<br><br>PERIOD OF STUDY IN COLLEGE : 5 years<br><br>ACADEMIC QUALIFICATION : UNDERGRAD<br><br>OCCUPATION : Intern at ALPHABET Inc.<br><br>HOBBY : Programming, Creating Drama, Photography, Eve teasing.<br><br>Contact no. : 8657867500<br><br>EMAIL ID : raghav17555@mechyd.ac.in</h2>");
-		JScrollPane sp = new JScrollPane(web);
-		sp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-		sp.setPreferredSize(new Dimension(900,900));
-		sp.setMinimumSize(new Dimension(60,60));
-		p1.add(sp,BorderLayout.CENTER);
-		frr.setTitle("DIRECTORY");
-		frr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frr.setVisible(true);
-		frr.setSize(900,900);
-		frr.add(p1);
-	}*/
 	public void actionPerformed(ActionEvent ae){
 		if(ae.getSource()==b0){
 			Ragh rg = new Ragh();
